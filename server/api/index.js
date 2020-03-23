2f1c01de33405b84536c64e8bfe1fe2d57509eca
Stacey Eliuk <stacey.eliuk@gmail.com>
2020-03-23 15:40:53 -0400

    fix: babel config

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
 router.use('/users', require('./users'))
+router.use('/companies', require('./companies'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
