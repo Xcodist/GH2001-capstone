08905707e6b1f1695b6b9f030916f40d1d103a32
Yumiko <yumikomannarelli@gmail.com>
2020-03-26 16:05:01 -0400

    background script

--- chrome/background.js
+++ chrome/background.js
@@ -1,10 +1,3 @@
-var background = {
-  init: function() {
 
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-      console.log('message received', request);
-    })
-  }
-};
 
-background.init();
+
