2676bbb81d0197fa385b3817d61444428f87d42a
Stacey Eliuk <stacey.eliuk@gmail.com>
2020-03-27 17:08:19 -0400

    chore: added additional logic to google shopping search to return lowest priced item

--- server/api/googleShopping.js
+++ server/api/googleShopping.js
@@ -2,39 +2,36 @@ require("../../secrets");
 const GSR = require("google-search-results-nodejs");
 let client = new GSR.GoogleSearchResults('c88292a99c393afebed7524cf431848fbf998b0cbfe2654a81b50525dae23148');
 
-client.json(
-  {
-    engine: "google",
-    q: "nintendo switch",
-    google_domain: "google.com",
-    tbm: "shop",
-    hl: "en",
-    gl: "us",
-    location: "United States"
-  },
-  result => {
-    const filtered = result.shopping_results.map((item) => {
-      if (item.source !== "Walmart" && item.source !== "Target" && item.source !== "Amazon") {
-        return item
-      }
-    })
-    console.log(filtered)
-  }
-);
-
-// var parameter = {
-//     engine: "google",
-//     q: "nintendo switch",
-//     google_domain: "google.com",
-//     tbm: "shop",
-//     hl: "en",
-//     gl: "us",
-//     location: "United States"
-// };
-
-// var callback = function(data) {
-//   console.log(data)
-// }
-
-// // Show result as JSON
-// client.json(parameter, callback)
+const productAr = ['nintendo switch', 'Nikon D3500 W/ AF-P DX NIKKOR 18-55mm f/3.5-5.6G VR Black']
+let altProducts = []
+for (let i = 0; i < productAr.length; i++) {
+  client.json(
+    {
+      engine: "google",
+      q: `${productAr[i]}`,
+      google_domain: "google.com",
+      tbm: "shop",
+      hl: "en",
+      gl: "us",
+      location: "United States"
+    },
+    result => {
+      debugger
+      let lowestPrice = {}
+      const brandFilter = result.shopping_results.map((item) => {
+        if (item.source !== "Walmart" && item.source !== "Target" && item.source !== "Amazon") {
+          if (Object.keys(lowestPrice).length === 0) {
+            lowestPrice = item
+          } else {
+            if (item.extracted_price < lowestPrice.extracted_price) {
+              lowestPrice = item
+            }
+          }
+          return item
+        }
+      })
+      altProducts.push(lowestPrice)
+      console.log(altProducts)
+    }
+  );
+}
