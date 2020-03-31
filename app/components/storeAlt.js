b6374fe72f07758968342239e3228f62285fbad2
Beth Howell <beahowel117@gmail.com>
2020-03-31 12:06:45 -0400

    refactor: added routes and navbar icon for store alternative

--- app/components/storeAlt.js
+++ app/components/storeAlt.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import axios from 'axios';
 
-export default class Store extends Component {
+export default class Stores extends Component {
 
   render(){
     return (
