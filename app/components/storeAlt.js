4a0b6a39793864498cfcae6ce6bbe2fb4a045361
Beth Howell <beahowel117@gmail.com>
2020-03-31 11:53:27 -0400

    fix: issue with material ui installation

---
+++ app/components/storeAlt.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import axios from 'axios';
+
+export default class Store extends Component {
+
+  render(){
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
