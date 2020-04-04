4e862a97c9887102bd3b0fd914fcc72e3fee4fc5
Aleksandra Bardymova <bardymovasasha@gmail.com>
2020-04-04 09:00:53 -0400

    fix: can see wishlist in popup and remove items

---
+++ chrome/background.js
@@ -0,0 +1,74 @@
+
+// window.open("index.html", "extension_popup", "width=300,height=400,status=no,scrollbars=yes,resizable=no");
+
+// chrome.windows.onFocusChanged.addListener(function(){
+//   chrome.tabs.query({
+//     active: true,
+//     currentWindow: true
+//   }, function(tabs) {
+//     var tab = tabs[0];
+//     var url = tab.url;
+//     console.log(tab);
+//     console.log('this is the url', url)
+//     if(url.includes('cart'|| 'basket' || 'shopping-cart')) {
+//       console.log('cart is recognized')
+//     } else {
+//       console.log('not in cart')
+//     }
+//   });
+// });
+// chrome.webNavigation.onCompleted.addListener(function() {
+//   chrome.tabs.query({
+//         active: true,
+//         currentWindow: true
+//       }, function(tabs) {
+//         var tab = tabs[0];
+//         var url = tab.url;
+//         console.log(tab);
+//         console.log('this is the url', url)
+//         if(url.includes('cart'|| 'basket' || 'shopping-cart')) {
+//         alert("This is my favorite website!");
+//         } else {
+//           alert("This is my favorite website!");
+//         }
+//       });
+//     });
+
+//
+
+const options = {
+    type: "basic",
+    title: "popup",
+    message: "You have more choices!",
+    iconUrl: "icons/colored-cart.png"
+  };
+  chrome.notifications.create(options);
+  
+  // function callback(){
+  //  console.log('popup done!')
+  // }
+  
+  // chrome.notifications.onClicked.addListener(redirectWindow);
+  // chrome.windows.onFocusChanged.addListener(redirectWindow);
+  
+  // function redirectWindow(){
+  //   alert("items")
+  // }
+  
+  chrome.windows.onFocusChanged.addListener(redirectWindow);
+  
+  function redirectWindow() {
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true
+      },
+      function(tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        if (url.includes("cart")) {
+          alert("Your alternate cart is now ready for checkout!");
+        }
+      }
+    );
+  }
---
+++ chrome/content.js
@@ -0,0 +1,44 @@
+
+if(window.location.href.includes('amazon')) {
+    if (window.location.href.includes('cart')) {
+      let items = document.getElementsByClassName('a-size-medium sc-product-title')
+      let prices = document.getElementsByClassName('a-size-medium a-color-price sc-price sc-white-space-nowrap sc-product-price sc-price-sign a-text-bold')
+      let cartList = []
+      for (let i = 0; i < prices.length; i++) {
+        let item = items[i].innerText;
+        let price = prices[i].innerText.slice(1)
+        cartList.push({[item]: price})
+      }
+      chrome.storage.local.set({"items": cartList}, function() {
+        console.log('Value is set to ' + cartList);
+      });
+    }
+    if (window.location.href.includes('buy')) {
+      let items = document.getElementsByClassName('a-row breakword')
+      let prices = document.querySelectorAll('.a-color-price .a-text-bold')
+      let cartList = []
+      for (let i = 0; i < prices.length; i++) {
+        let item = items[i].innerText;
+        let price = prices[i].innerText.slice(1)
+        cartList.push({[item]: price})
+      }
+      chrome.storage.local.set({"items": cartList}, function() {
+        console.log('Value is set to ' + cartList);
+      });
+    }
+  }
+  if(window.location.href.includes('walmart')) {
+    if (window.location.href.includes('cart')) {
+      let items = document.getElementsByClassName("cart-item-name js-product-title")
+      let prices = document.querySelectorAll('.price-main .price .visuallyhidden')
+      let cartList = []
+      for (let i = 0; i < prices.length; i++) {
+        let item = items[i].innerText;
+        let price = prices[i].innerText.slice(1)
+        cartList.push({[item]: price})
+      }
+      chrome.storage.local.set({"items": cartList}, function() {
+        console.log('Value is set to ' + cartList);
+      });
+    }
+  }
