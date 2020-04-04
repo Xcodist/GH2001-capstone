0bda45ad22d5acd19465adafc74dc13826803609
Aleksandra Bardymova <bardymovasasha@gmail.com>
2020-04-04 14:16:13 -0400

    feature: categories of alt websites

--- app/components/ForHome.js
+++ app/components/ForHome.js
@@ -3,26 +3,25 @@ import React, { Component } from "react";
 export default class ForHome extends Component {
   render() {
     return (
-      <div className="mainBody">
         <ul className="nobull">
-          <li>
-            <a
-              className="stores"
-              onClick={() => {
-                window.open("https://www.columbia.com/");
-              }}
-            >
-              Columbia
-            </a>
-            <img
-              className="columbia"
-              src="https://columbia.scene7.com/is/image/ColumbiaSportswear2/03-02_24694_COL_US_Mens_Dry_NewArrivals_SuperCat_HikingSubhero_V4_M?fmt=pjpeg&scl=1"
-              width="200"
-              height="100"
-            />
-          </li>
-        </ul>
-      </div>
-    );
-  }
+        <li className="storeLink">
+          <a
+            className="stores"
+            onClick={() => {
+              window.open("https://www.aptdeco.com/");
+            }}
+          >
+            AptDeco
+          </a>
+          <br />
+          <img
+            className="aptDeco"
+            src="https://lh3.googleusercontent.com/proxy/KgEMg13aZo9XFo_Nh5H0hgz36B7fVMvfwjg6RHoYxplYqC_hlljomteQTr5sNuGUL4MxletTzaSKdLMdyAB2MKY_35KAvoyPcf-RAEj5NXzQj4AjfbJwmIcHkvMWEw"
+            width="200"
+            height="100"
+          />
+        </li>
+      </ul>
+  );
+}
 }
