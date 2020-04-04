aea9a5b58c1b99f99bdf9e232103be46045a251b
Aleksandra Bardymova <bardymovasasha@gmail.com>
2020-04-04 14:49:52 -0400

    categories of alternate stores

--- app/components/Electronics.js
+++ app/components/Electronics.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 export default class Electronics extends Component {
   render() {
     return (
+        <div className="mainBody">
         <ul className="nobull">
         <li className="storeLink">
           <a
@@ -21,7 +22,42 @@ export default class Electronics extends Component {
             height="100"
           />
         </li>
+        <li className="storeLink">
+          <a
+            className="stores"
+            onClick={() => {
+              window.open("https://www.lenovo.com/us/en?Redirect=False");
+            }}
+          >
+            Lenovo
+          </a>
+          <br />
+          <img
+            className="lenovo"
+            src="https://www.lenovo.com/medias/C930688B-D132-455C-AD12-007504A60117.jpg?context=bWFzdGVyfHJvb3R8MTE1Njh8aW1hZ2UvanBlZ3xoNzAvaGI4LzEwNTkxNjQ2NzExODM4LmpwZ3xiODAxMzYxYTNiNzU1YWRhYjFkZWUwMTc2NjUyNDllNzZkNmQwYmUwZWQ0MTRhZjFjNjdkMThlOGQ0MjRjMmNk"
+            width="200"
+            height="100"
+          />
+        </li>
+        <li className="storeLink">
+          <a
+            className="stores"
+            onClick={() => {
+              window.open("https://www.lg.com/us");
+            }}
+          >
+            LG
+          </a>
+          <br />
+          <img
+            className="microsoft"
+            src="https://irp-cdn.multiscreensite.com/bf81ee8e/dms3rep/multi/mobile/LG-Appliance-Repair-650x414.jpg"
+            width="200"
+            height="100"
+          />
+        </li>
       </ul>
+      </div>
   );
 }
 }
