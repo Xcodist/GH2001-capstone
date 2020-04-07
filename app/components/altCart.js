87daf1058363177ae86289160648915c3fa3a345
Stacey Eliuk <stacey.eliuk@gmail.com>
2020-04-07 10:22:57 -0400

    fix: altCart conditional load

--- app/components/altCart.js
+++ app/components/altCart.js
@@ -2,11 +2,58 @@ import React, { Component } from "react";
 import {fetchAlternatives} from '../store/alt'
 import {addToWishlistThunk} from '../store/wishlist'
 import { connect } from "react-redux";
+
+const hasAlt = (alt, user) => {
+  return (
+    <div className='mainBody'>
+    <div className='header'>Your altCart:</div>
+    {alternatives.map(alternative => {
+      return (
+        <div key={alternative.title}>
+          <div className="altProd">
+            <div className="leftCol">
+              <img className="thumbnail" src={alternative.thumbnail} />
+            </div>
+            <div className="rightCol">
+              <a className="prodTitle"
+              onClick={() => {
+                window.open(alternative.link)
+              }}>
+                {alternative.title}
+              </a>
+              <div className="prodInfo">
+                {alternative.source}<br/>
+                {alternative.price}<br/>
+                {alternative.snippet}<br/>
+              </div>
+              <button onClick={() => {
+                this.handleSubmit(event, alternative, user)
+              }}>Add to wishlist!</button>
+            </div>
+          </div>
+        </div>
+      );
+    })}
+  </div>
+  )
+}
+
+const noCart = () => {
+  return (
+    <div className='noCart'>
+      <h1>You have no items in your cart!</h1>
+      <p>Add some items to your cart to see alternatives.</p>
+    </div>
+  )
+}
+
+
 class AltCart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cartFetched: false
+      cartFetched: false,
+      itemsInCart: true
     }
 
   }
@@ -29,12 +76,29 @@ class AltCart extends Component {
       }
       return parsed
     })
-    this.props.fetchAlternatives(cart, prices)
+    if (cart.length > 0) {
+      this.props.fetchAlternatives(cart, prices);
+    } else {
+      this.setState({
+        ...this.state,
+        itemsInCart: false
+      })
+    }
   }
 
   handleSubmit(event, alternative, user) {
     event.preventDefault()
     this.props.addToWishlistThunk(alternative, user)
+  }
+
+  altCartOptions() {
+    const alternatives = this.props.state.alt
+    const user = this.props.state.user;
+    if (alternatives.length > 0) {
+      return hasAlt(alternatives, user)
+    } else if (!cart.length) {
+      return
+    }
   }
 
   render() {
