f7ebe0721e5dcfb967ceecfa13752f9221d4e83e
Beth Howell <beahowel117@gmail.com>
2020-04-06 23:06:38 -0400

    style: styled stores

--- app/components/Electronics.js
+++ app/components/Electronics.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Electronics extends Component {
   render() {
     return (
-        <div className="mainBody">
+        <div className="mainStore">
         <ul className="nobull">
+        <div className="store-container">
         <li className="storeLink">
-          <a
+          <br />
+          <img
+            className="acer"
+            src="https://vsa9.com/wp-content/uploads/2019/03/544207-meet-the-acer-swift-5.jpg"
+            width="84"
+            height="84"
+          />
+             <a
             className="stores"
             onClick={() => {
               window.open("https://www.acer.com/ac/en/US/content/home");
@@ -14,16 +23,18 @@ export default class Electronics extends Component {
           >
             Acer
           </a>
+        </li>
+        </div>
+        <div className="store-container">
+        <li className="storeLink">
           <br />
           <img
-            className="acer"
-            src="https://vsa9.com/wp-content/uploads/2019/03/544207-meet-the-acer-swift-5.jpg"
-            width="200"
-            height="100"
+            className="lenovo"
+            src="https://www.lenovo.com/medias/C930688B-D132-455C-AD12-007504A60117.jpg?context=bWFzdGVyfHJvb3R8MTE1Njh8aW1hZ2UvanBlZ3xoNzAvaGI4LzEwNTkxNjQ2NzExODM4LmpwZ3xiODAxMzYxYTNiNzU1YWRhYjFkZWUwMTc2NjUyNDllNzZkNmQwYmUwZWQ0MTRhZjFjNjdkMThlOGQ0MjRjMmNk"
+            width="84"
+            height="84"
           />
-        </li>
-        <li className="storeLink">
-          <a
+            <a
             className="stores"
             onClick={() => {
               window.open("https://www.lenovo.com/us/en?Redirect=False");
@@ -31,16 +42,18 @@ export default class Electronics extends Component {
           >
             Lenovo
           </a>
+        </li>
+        </div>
+        <div className="store-container">
+        <li className="storeLink">
           <br />
           <img
-            className="lenovo"
-            src="https://www.lenovo.com/medias/C930688B-D132-455C-AD12-007504A60117.jpg?context=bWFzdGVyfHJvb3R8MTE1Njh8aW1hZ2UvanBlZ3xoNzAvaGI4LzEwNTkxNjQ2NzExODM4LmpwZ3xiODAxMzYxYTNiNzU1YWRhYjFkZWUwMTc2NjUyNDllNzZkNmQwYmUwZWQ0MTRhZjFjNjdkMThlOGQ0MjRjMmNk"
-            width="200"
-            height="100"
+            className="microsoft"
+            src="https://irp-cdn.multiscreensite.com/bf81ee8e/dms3rep/multi/mobile/LG-Appliance-Repair-650x414.jpg"
+            width="84"
+            height="84"
           />
-        </li>
-        <li className="storeLink">
-          <a
+             <a
             className="stores"
             onClick={() => {
               window.open("https://www.lg.com/us");
@@ -48,15 +61,10 @@ export default class Electronics extends Component {
           >
             LG
           </a>
-          <br />
-          <img
-            className="microsoft"
-            src="https://irp-cdn.multiscreensite.com/bf81ee8e/dms3rep/multi/mobile/LG-Appliance-Repair-650x414.jpg"
-            width="200"
-            height="100"
-          />
         </li>
+        </div>
       </ul>
+      <Link className='back' to="/search">Back</Link>
       </div>
   );
 }
--- app/components/ForHome.js
+++ app/components/ForHome.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class ForHome extends Component {
   render() {
     return (
-        <div className="mainBody">
+        <div className="mainStore">
         <ul className="nobull">
+        <div className="store-container">
         <li className="storeLink">
-          <a
+          <br />
+          <img
+            className="aptDeco"
+            src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-1/c31.31.387.387a/s100x100/999124_231196013695603_833984939_n.png?_nc_cat=105&_nc_sid=dbb9e7&_nc_oc=AQnkUV6pqXNje595cZTsh-dhwR209Ce-fErHTnfV3QNVEsJH3TK6SKQcURcPSakT5jig9TyXFNrsalmascHNSonh&_nc_ht=scontent-lga3-1.xx&oh=e1c794636fb089bdda6a90b1bc40822c&oe=5EAD30D7"
+            width="84"
+            height="84"
+          />
+           <a
             className="stores"
             onClick={() => {
               window.open("https://www.aptdeco.com/");
@@ -14,16 +23,18 @@ export default class ForHome extends Component {
           >
             AptDeco
           </a>
+        </li>
+        </div>
+        <div className="store-container">
+        <li className="storeLink">
           <br />
           <img
-            className="aptDeco"
-            src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-1/c31.31.387.387a/s100x100/999124_231196013695603_833984939_n.png?_nc_cat=105&_nc_sid=dbb9e7&_nc_oc=AQnkUV6pqXNje595cZTsh-dhwR209Ce-fErHTnfV3QNVEsJH3TK6SKQcURcPSakT5jig9TyXFNrsalmascHNSonh&_nc_ht=scontent-lga3-1.xx&oh=e1c794636fb089bdda6a90b1bc40822c&oe=5EAD30D7"
-            width="150"
-            height="100"
+            className="honest"
+            src="https://i.imgur.com/W7vnBVs.jpg"
+            width="84"
+            height="84"
           />
-        </li>
-        <li className="storeLink">
-          <a
+           <a
             className="stores"
             onClick={() => {
               window.open("https://www.honest.com/");
@@ -31,15 +42,17 @@ export default class ForHome extends Component {
           >
             Honest
           </a>
+        </li>
+        </div>
+        <div className="store-container">
+        <li className="storeLink">
           <br />
           <img
-            className="honest"
-            src="https://i.imgur.com/W7vnBVs.jpg"
-            width="200"
-            height="100"
-          />
-        </li>
-        <li className="storeLink">
+            className="goodee"
+            src="https://cdn.shopify.com/s/files/1/0071/1684/9252/products/GOODEE_GoodeeTote_Yellow_2_1500x.jpg?v=1574794842"
+            width="84"
+            height="84"
+            />
           <a
             className="stores"
             onClick={() => {
@@ -48,15 +61,10 @@ export default class ForHome extends Component {
           >
             Goodee
           </a>
-          <br />
-          <img
-            className="goodee"
-            src="https://cdn.shopify.com/s/files/1/0071/1684/9252/products/GOODEE_GoodeeTote_Yellow_2_1500x.jpg?v=1574794842"
-            width="200"
-            height="200"
-            />
         </li>
+        </div>
       </ul>
+      <Link className='back' to="/search">Back</Link>
       </div>
   );
 }
