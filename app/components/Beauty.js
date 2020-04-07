90978773b270aae342db86adf08c54afe5d21f5e
Beth Howell <beahowel117@gmail.com>
2020-04-07 12:00:14 -0400

    style: added wrapping of stores

--- app/components/Beauty.js
+++ app/components/Beauty.js
@@ -4,71 +4,78 @@ import { Link } from "react-router-dom";
 export default class Beauty extends Component {
   render() {
     return (
-        <div className="mainStore">
+      <div>
+         <Link className="back" to="/search">
+          Back
+        </Link>
         <ul className="nobull">
-        <div className="store-container">
-        <li className="storeLink">
-          <br />
-          <img
-            className="lush"
-            className="logoContainer"
-            src="https://i.guim.co.uk/img/media/4794451cf6d1065daf74af3d0d50b77bbbd83f3e/362_163_3454_2073/master/3454.jpg?width=620&quality=85&auto=format&fit=max&s=0cec0d93c9c7deb5e9ee7b7325cd940c"
-            width="84"
-            height="84"
-          />
-            <a
-            className="stores"
-            onClick={() => {
-              window.open("https://www.lushusa.com/");
-            }}
-          >
-            Lush
-          </a>
-        </li>
-        </div>
-        <div className="store-container">
-        <li className="storeLink">
-          <br />
-          <img
-          className="youth"
-          className="logoContainer"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_Rk2RWkBTFQ1lqSoOVjR4wEWSPqxQDs2pIXeUNWvi3-IZdw1p&usqp=CAU"
-          width="84"
-          height="84"
-          />
-             <a
-            className="stores"
-            onClick={() => {
-              window.open("https://www.youthtothepeople.com/")
-            }}
-          >
-            Youth To The People
-          </a>
-        </li>
-        </div>
-        <div className="store-container">
-        <li className="storeLink">
-          <br />
-          <img
-          className="rahua"
-          className="logoContainer"
-          src="https://cdn.shopify.com/s/files/1/0067/5266/5673/files/RAHUA-LOGO-RainforestGrownBeauty-Horizontal-BROWN-FINAL_170x.png?v=1542047302"
-          width="84"
-          height="84"
-          />
-            <a
-            className="stores"
-            onClick={() => {
-              window.open("https://rahua.com/")
-            }}
-            >
-            Rahua
-          </a>
-        </li>
-        </div>
-      </ul>
-      <Link className='back' to="/search">Back</Link>
+          <div className="mainStore">
+            <div className="store-container">
+              <li className="storeLink">
+                <br />
+                <img
+                  className="lush"
+                  className="logoContainer"
+                  src="https://pbs.twimg.com/profile_images/1120784179182989312/uucySY3-_400x400.png"
+                  width="84"
+                  height="84"
+                />
+                <a
+                  className="stores"
+                  onClick={() => {
+                    window.open("https://www.lushusa.com/");
+                  }}
+                >
+                  Lush
+                </a>
+              </li>
+            </div>
+            <div className="store-container">
+              <li className="storeLink">
+                <br />
+                <img
+                  className="youth"
+                  className="logoContainer"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_Rk2RWkBTFQ1lqSoOVjR4wEWSPqxQDs2pIXeUNWvi3-IZdw1p&usqp=CAU"
+                  width="84"
+                  height="84"
+                />
+                <a
+                  className="stores"
+                  onClick={() => {
+                    window.open("https://www.youthtothepeople.com/");
+                  }}
+                >
+                  Youth To The People
+                </a>
+              </li>
+            </div>
+            <div className="store-container">
+              <li className="storeLink">
+                <br />
+                <img
+                  className="rahua"
+                  className="logoContainer"
+                  src="https://cdn.shopify.com/s/files/1/0067/5266/5673/files/RAHUA-LOGO-RainforestGrownBeauty-Horizontal-BROWN-FINAL_170x.png?v=1542047302"
+                  width="84"
+                  height="84"
+                />
+                <a
+                  className="stores"
+                  onClick={() => {
+                    window.open("https://rahua.com/");
+                  }}
+                >
+                  Rahua
+                </a>
+              </li>
+            </div>
+          </div>
+        </ul>
+        {/* <Link className="back" to="/search">
+          Back
+        </Link> */}
       </div>
-  );
-}
+    );
+  }
 }
--- app/components/Clothing.js
+++ app/components/Clothing.js
@@ -4,105 +4,107 @@ import { Link } from "react-router-dom";
 export default class Clothing extends Component {
   render() {
     return (
-      <div className="mainStore">
+      <div>
+        <Link className="back" to="/search">
+          Back
+        </Link>
         <ul className="nobull">
-        <div className="store-container">
-          <li className="storeLink">
-            <br />
-            <img
-              className="columbia"
-              src="https://columbia.scene7.com/is/image/ColumbiaSportswear2/03-02_24694_COL_US_Mens_Dry_NewArrivals_SuperCat_HikingSubhero_V4_M?fmt=pjpeg&scl=1"
-              height ="84"
-              width="84"
-            />
-             <a
-              className="stores"
-              onClick={() => {
-                window.open("https://www.columbia.com/");
-              }}
-            >
-              Columbia
-            </a>
-          </li>
-          </div>
-          <div className="store-container">
-          <li className="storeLink">
-            <br />
-            <img
-              src="https://onda.org/wp-content/uploads/2017/11/Patagonia-logo.png"
-              height ="84"
-              width="84"
-            />
-            <a
-              className="stores"
-              onClick={() => {
-                window.open("http://www.patagonia.com/home/");
-              }}
-            >
-              Patagonia
-            </a>
-          </li>
-          </div>
-          <div className="store-container">
-          <li className="storeLink">
-            <br />
-            <img
-              src="https://d1bntizjlw65va.cloudfront.net/87784eefec72d66cbad069899ffde3492576b5f3.jpg"
-              height ="84"
-              width="84"
-            />
-              <a
-              className="stores"
-              onClick={() => {
-                window.open("http://www.thereformation.com/");
-              }}
-            >
-              Reformation
-            </a>
-          </li>
-          </div>
-          <div className="store-container">
-          <li className="storeLink">
-          <div className="store-item">
+          <div className="mainStore">
+            <div className="store-container">
+              <li className="storeLink">
+                <br />
+                <img
+                  className="columbia"
+                  src="https://i.pinimg.com/originals/0a/b8/da/0ab8dae2ba3543f6043ac661db9f6101.png"
+                  height="84"
+                  width="84"
+                />
+                <a
+                  className="stores"
+                  onClick={() => {
+                    window.open("https://www.columbia.com/");
+                  }}
+                >
+                  Columbia
+                </a>
+              </li>
             </div>
-            <br />
-            <img
-              src="https://d2bo3wgtyrxq4p.cloudfront.net/client_info/FREAKSSTORE/itemimage/christydawnjpg.jpg"
-              height ="84"
-              width="84"
-            />
-              <a
-              className="stores"
-              onClick={() => {
-                window.open("https://christydawn.com/");
-              }}
-            >
-              Christy Dawn
-            </a>
-          </li>
-          </div>
-          <div className="store-container">
-          <li className="storeLink">
-          <div className="store-item">
+            <div className="store-container">
+              <li className="storeLink">
+                <br />
+                <img
+                  src="https://i.ebayimg.com/images/g/aTwAAOSwfu9dfX4u/s-l300.jpg"
+                  height="84"
+                  width="84"
+                />
+                <a
+                  className="stores"
+                  onClick={() => {
+                    window.open("http://www.patagonia.com/home/");
+                  }}
+                >
+                  Patagonia
+                </a>
+              </li>
             </div>
-            <br />
-            <img
-              src="https://digital.hbs.edu/platform-digit/wp-content/uploads/sites/2/2018/04/Gap1-350x200.jpg"
-              height ="84"
-              width="84"
-            />
-              <a
-              className="stores"
-              onClick={() => {
-                window.open("https://www.gap.com/");
-              }}
-            >
-              GAP
-            </a>
-          </li>
+            <div className="store-container">
+              <li className="storeLink">
+                <br />
+                <img
+                  src="https://d1bntizjlw65va.cloudfront.net/87784eefec72d66cbad069899ffde3492576b5f3.jpg"
+                  height="84"
+                  width="84"
+                />
+                <a
+                  className="stores"
+                  onClick={() => {
+                    window.open("http://www.thereformation.com/");
+                  }}
+                >
+                  Reformation
+                </a>
+              </li>
+            </div>
+            <div className="store-container">
+              <li className="storeLink">
+                <div className="store-item"></div>
+                <br />
+                <img
+                  src="https://d2bo3wgtyrxq4p.cloudfront.net/client_info/FREAKSSTORE/itemimage/christydawnjpg.jpg"
+                  height="84"
+                  width="84"
+                />
+                <a
+                  className="stores"
+                  onClick={() => {
+                    window.open("https://christydawn.com/");
+                  }}
+                >
+                  Christy Dawn
+                </a>
+              </li>
+            </div>
+            <div className="store-container">
+              <li className="storeLink">
+                <div className="store-item"></div>
+                <br />
+                <img
+                  src="https://digital.hbs.edu/platform-digit/wp-content/uploads/sites/2/2018/04/Gap1-350x200.jpg"
+                  height="84"
+                  width="84"
+                />
+                <a
+                  className="stores"
+                  onClick={() => {
+                    window.open("https://www.gap.com/");
+                  }}
+                >
+                  GAP
+                </a>
+              </li>
+            </div>
           </div>
         </ul>
-        <Link className='back' to="/search">Back</Link>
       </div>
     );
   }
