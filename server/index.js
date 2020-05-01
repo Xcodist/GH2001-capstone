925b41c4d2b8234b1b6ebaf420d3689d503cb0a7
Yumiko <yumikomannarelli@gmail.com>
2020-04-30 21:03:23 -0400

    added env

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const db = require('./db')
 const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
+require('dotenv').config()
 module.exports = app
 
 // This is a global Mocha hook, used for resource cleanup.
