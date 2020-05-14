9d2cab5b9598f1079183ef890c23eec7ec760b9e
Yumiko <yumikomannarelli@gmail.com>
2020-05-14 12:22:32 -0400

    changed file paths to heroku app

--- client/app/store/wishlist.js
+++ client/app/store/wishlist.js
@@ -24,7 +24,7 @@ const removeFromWishlist = wishlistId => ({
 
 export const getWishlistThunk = (userId) => async dispatch => {
   try {
-    const wishlist = await Axios.get(`http://localhost:8080/api/wishlist/${userId}`)
+    const wishlist = await Axios.get(`https://altcart.herokuapp.com/api/wishlist/${userId}`)
     console.log('this is wishlist', wishlist.data[0].wishlists)
     dispatch(getWishlist(wishlist.data[0].wishlists));
   } catch(e) {
@@ -35,7 +35,7 @@ export const getWishlistThunk = (userId) => async dispatch => {
 
 export const addToWishlistThunk = (altItem, user) => async dispatch => {
   try {
-    const newItem = await Axios.put(`/api/wishlist/add/${user.id}`, altItem);
+    const newItem = await Axios.put(`https://altcart.herokuapp.com/api/wishlist/add/${user.id}`, altItem);
     if(newItem) {
       dispatch(addToWishlist(newItem.data))
     } else {
@@ -49,7 +49,7 @@ export const addToWishlistThunk = (altItem, user) => async dispatch => {
 
 export const removeFromWishlistThunk = ( userId, wishlistId) => async dispatch => {
   try {
-    const removedItem = await Axios.delete(`/api/wishlist/remove/${userId}/${wishlistId}`);
+    const removedItem = await Axios.delete(`https://altcart.herokuapp.com/api/wishlist/remove/${userId}/${wishlistId}`);
     dispatch(removeFromWishlist(wishlistId));
   }  catch (e) {
     console.log('Problem with removing of item from your wishlist',e)
